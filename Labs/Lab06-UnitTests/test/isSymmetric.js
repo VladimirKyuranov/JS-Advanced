70de2a662d9c77e187435daa40339e2b59a98823
Vladimir Kyuranov <the_crow@abv.bg>
2018-10-30 09:45:05 +0200

    Update Labs/Lab06-UnitTests/test/isSymmetric.js

--- Labs/Lab06-UnitTests/test/isSymmetric.js
+++ Labs/Lab06-UnitTests/test/isSymmetric.js
@@ -4,8 +4,7 @@ function isSymmetric(arr) {
     if (!Array.isArray(arr))
         return false; // Non-arrays are non-symmetric
     let reversed = arr.slice(0).reverse(); // Clone and reverse
-    let equal = (JSON.stringify(arr) === JSON.stringify(reversed));
-    return equal;
+    return (JSON.stringify(arr) === JSON.stringify(reversed));
 }
 
 describe("isSymmetric Tests", function () {
